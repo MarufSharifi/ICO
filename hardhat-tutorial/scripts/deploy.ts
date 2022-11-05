40fd681e259fd76411463e08e9c08201209e38f5
Maruf Sharifi <maruf@truetale.io>
2022-11-05 22:28:04 +0430

    created new script to deploy our crypto dev token contract

--- hardhat-tutorial/scripts/deploy.ts
+++ hardhat-tutorial/scripts/deploy.ts
@@ -1,23 +1,31 @@
 import { ethers } from "hardhat";
+import dotenv from 'dotenv';
+import {CRYPTO_DEVS_NFT_CONTRACT_ADDRESS} from '../constants';
+
+
+dotenv.config({path: ".env"});
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    const cryptoDevsNftContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+    const CryptoDevTokenContract = await ethers.getContractFactory('CryptoDevToken');
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    const deployedCryptoDevContractAddress = await CryptoDevTokenContract.deploy(cryptoDevsNftContract);
+
+    await deployedCryptoDevContractAddress.deployed();
+
+
+    console.log('Crypto Devs Token Contract Address:', deployedCryptoDevContractAddress.address);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().then(() => process.exit(0))
+.catch((error) => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1)
 });
