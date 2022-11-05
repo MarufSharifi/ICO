d733d956fac9c83e628276333e2ea431ac49b638
Maruf Sharifi <maruf@truetale.io>
2022-11-05 22:34:11 +0430

    edited hardhat.config.ts for sake of our contract https url and my private key

--- hardhat-tutorial/hardhat.config.ts
+++ hardhat-tutorial/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+dotenv.config({path: '.env'})
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
